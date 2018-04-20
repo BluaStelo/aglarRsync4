65c0ebc36760f45f536bc3eb0e4fcc611e4d7d40
Romain from Blua Stelo <romain@bluastelo.com>
2018-04-20 02:51:30 +0200

    Initial Commit

---
+++ aglarRsync4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  aglarRsync4
+//
+//  Created by Romain Bourgin on 20/04/2018.
+//  Copyright © 2018 Blua Stelo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ aglarRsync4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aglarRsync4
+//
+//  Created by Romain Bourgin on 20/04/2018.
+//  Copyright © 2018 Blua Stelo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
